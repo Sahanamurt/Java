6afe2e517c25ef151b942bdd07449ec798a8f99c
Sahanamurt <105217467+Sahanamurt@users.noreply.github.com>
2025-04-09 17:24:13 +0530

    hiuuu

--- src/main/java/test/App.java
+++ src/main/java/test/App.java
@@ -5,6 +5,6 @@ package test;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World h jji");
+        System.out.println("Hello World huuuuji");
     }
 }
